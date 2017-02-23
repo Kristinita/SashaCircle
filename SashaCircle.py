344cdd2f80e722e987681c80ba7f00b472d9d684
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2017-02-23 13:07:11 +0300

    [Test] Upgrade Python version

--- SashaCircle.py
+++ SashaCircle.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # @Author: SashaChernykh
 # @Date: 2017-02-23 07:57:46
-# @Last Modified time: 2017-02-23 12:57:09
+# @Last Modified time: 2017-02-23 13:06:53
 
 print("Sasha Goddess!")
